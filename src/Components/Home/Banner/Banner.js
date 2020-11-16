f4a88e31c63617b673513d5ac0646f11ab3c4e05
Shakib448 <shakiba448@gmail.com>
2020-11-16 09:50:47 +0600

    Search form added

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,12 +1,36 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Form, Col, Button } from "react-bootstrap";
 import "./Banner.sass";
 
 const Banner = () => {
   return (
     <>
       <section className="banner">
-        <Container></Container>
+        <Container>
+          <Row className="banner__form">
+            <h1>Find Your House Rent</h1>
+
+            <Form>
+              <Form.Row className="align-items-center">
+                <Col xs="auto">
+                  <Form.Label htmlFor="inlineFormInput" srOnly>
+                    Name
+                  </Form.Label>
+                  <Form.Control
+                    className="mb-2"
+                    id="inlineFormInput"
+                    placeholder="Search..."
+                  />
+                </Col>
+                <Col xs="auto">
+                  <Button type="submit" className="mb-2 banner__searchBtn">
+                    Find Now
+                  </Button>
+                </Col>
+              </Form.Row>
+            </Form>
+          </Row>
+        </Container>
       </section>
     </>
   );
