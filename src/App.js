9f9a9c9f4eb198a7cfd484a085cdd6ec77ad9a82
Shakib448 <shakiba448@gmail.com>
2020-11-16 09:15:35 +0600

    Background image and overlay added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navigation from "../src/Components/Shared/Navigation/Navigation";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Components/Home/Home/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Navigation />
+        <Home />
       </Router>
     </>
   );
---
+++ src/Components/Home/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Banner.sass";
+
+const Banner = () => {
+  return (
+    <>
+      <section className="banner">
+        <Container></Container>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
