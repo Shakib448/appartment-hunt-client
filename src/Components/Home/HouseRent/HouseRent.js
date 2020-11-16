c7341d6ec436afd1c1f6c82b1dd3c2447595d481
Shakib448 <shakiba448@gmail.com>
2020-11-16 10:38:27 +0600

    Style componnet created

--- src/Components/Home/HouseRent/HouseRent.js
+++ src/Components/Home/HouseRent/HouseRent.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./HouseRent.sass";
+import test from "../../../images/Rectangle 394.png";
 
 const HouseRent = () => {
   return (
@@ -13,7 +14,35 @@ const HouseRent = () => {
             <h1>available tody</h1>
           </div>
         </Row>
-        <Row className="justify-content-center mt-3 mb-3"></Row>
+        <Row className="justify-content-center mt-3 mb-3">
+          <Col md={4} sm={12}>
+            <Card className="houseRent__card">
+              <Card.Img variant="top" src={test} />
+              <Card.Body>
+                <Card.Title style={{ overflow: "hidden" }}>
+                  Card Title
+                </Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <div className="houseRent__cardBody">
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </div>
+                <div className="card__footer">
+                  <Button variant="primary">Go somewhere</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </section>
   );
