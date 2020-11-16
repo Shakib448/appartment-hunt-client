20c05d64fcfda5a7edec652ec225a261702848e9
Shakib448 <shakiba448@gmail.com>
2020-11-16 12:51:08 +0600

    Fake Data created

---
+++ src/FakeData/fakeData.js
@@ -0,0 +1,59 @@
+import img1 from "../images/Rectangle 394.png";
+import img2 from "../images/Rectangle 396.png";
+import img3 from "../images/Rectangle 398.png";
+import img4 from "../images/Rectangle 405.png";
+import img5 from "../images/Rectangle 403.png";
+import img6 from "../images/Rectangle 404.png";
+
+const rentHouse = [
+  {
+    img: img1,
+    title: "Washington Avenue",
+    location: "Nasirabad H/S, Chattogram",
+    bed: "3 Bedrooms",
+    bathRoom: "2 Bathroom",
+    price: 194,
+  },
+  {
+    img: img2,
+    title: "Family Apartment Three",
+    location: "Nasirabad H/S, Chattogram",
+    bed: "3 Bedrooms",
+    bathRoom: "2 Bathroom",
+    price: 356,
+  },
+  {
+    img: img3,
+    title: "Gorgeous house",
+    location: "Nasirabad H/S, Chattogram",
+    bed: "3 Bedrooms",
+    bathRoom: "2 Bathroom",
+    price: 256,
+  },
+  {
+    img: img4,
+    title: "Luxury Villa",
+    location: "Nasirabad H/S, Chattogram",
+    bed: "3 Bedrooms",
+    bathRoom: "2 Bathroom",
+    price: 345,
+  },
+  {
+    img: img5,
+    title: "Epic Huda Palacio",
+    location: "Nasirabad H/S, Chattogram",
+    bed: "3 Bedrooms",
+    bathRoom: "2 Bathroom",
+    price: 536,
+  },
+  {
+    img: img6,
+    title: "Washington Avenue",
+    location: "Nasirabad H/S, Chattogram",
+    bed: "3 Bedrooms",
+    bathRoom: "2 Bathroom",
+    price: 283,
+  },
+];
+
+export default rentHouse;
