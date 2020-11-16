b3a3b14d31024e68aaa96fefd3bf1fd4cd0fd9e1
Shakib448 <shakiba448@gmail.com>
2020-11-16 10:12:43 +0600

    Style componnet created

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import HouseRent from "../HouseRent/HouseRent";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <HouseRent />
     </>
   );
 };
---
+++ src/Components/Home/HouseRent/HouseRent.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import "./HouseRent.sass";
+
+const HouseRent = () => {
+  return (
+    <section>
+      <Container>
+        <Row className="justify-content-center m-4">
+          <div className="houseRend__content">
+            <h5>House Rent</h5>
+            <h1>Discover the latest Rent</h1>
+            <h1>available tody</h1>
+          </div>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HouseRent;
