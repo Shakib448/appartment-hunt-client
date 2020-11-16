d20a5aa3e2338416ba7fd4ef83cd45adc5bf8594
Shakib448 <shakiba448@gmail.com>
2020-11-16 06:29:53 +0600

    Branch deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
