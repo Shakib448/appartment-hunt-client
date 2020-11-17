7188f8eb01c3b74ea60fa28b616bd512dfb7ab07
Shakib448 <shakiba448@gmail.com>
2020-11-17 12:40:12 +0600

    Form completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home/Home";
 import Login from "./Components/Login/Login/Login";
 import Notfound from "./Components/NotFound/Notfound";
-import ApartmentDetails from "./Components/ApartmentDetails/ApartmentDetails";
+import Apartment from "./Components/Apartment/Apartment";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/apartment-details" component={ApartmentDetails} />
+          <Route exact path="/apartment-details" component={Apartment} />
           <Route exact path="*" component={Notfound} />
         </Switch>
       </Router>
